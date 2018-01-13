e12ce8edd7b184d8d99d71c51920270ff93d299b
Lucas Almeida <lucas.a.souza@outlook.com>
2018-01-12 22:03:55 -0200

    Add drawer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MUIDefaultTheme from './hoc/MUIDefaultTheme/MUIDefaultTheme';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class App extends Component {
+  state = {
+    open: false
+  }
+  
+  handleToggle = () => {
+    this.setState({open: !this.state.open});
+  }
+
   render() {
     return (
         <div className="App">
           <AppBar
             title="Title"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonClick={this.handleToggle}
           />
+          <Drawer 
+            open={this.state.open}
+            docked={false}
+            onRequestChange={(open) => {this.setState({open: open})}}>
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
         </div>
     );
   }
--- src/hoc/MUIDefaultTheme/MUIDefaultTheme.js
+++ src/hoc/MUIDefaultTheme/MUIDefaultTheme.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { MuiThemeProvider } from 'material-ui/styles';
 
 const muiDefaultTheme = (WrappedComponent) => {
